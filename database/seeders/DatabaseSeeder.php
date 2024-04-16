b0ff9836e8625238013df2732fa254e6230189e6
IngGonzalo <gonzalo_5806676@outlook.es>
2024-04-15 21:08:37 -0400

    seed user commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('roles')->insert([
+            "name" => "Admin",
+            "created_at" => Carbon::now(),
+            "updated_at" => Carbon::now(),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('roles')->insert([
+            "name" => "Customers",
+            "created_at" => Carbon::now(),
+            "updated_at" => Carbon::now(),
+        ]);
+
+         // OBTENER LOS ROLES DE LA DB
+         $roles = DB::table('roles')->get();
+
+         DB::table('users')->insert([
+            "name" => "Ramona Perez Flores",
+            "email" => "ramona@correo.com",
+            "ci" => 2845926,
+            "role_id" => $roles[0]->id,
+            "password" => Hash::make("Ramona123"),
+            "created_at" => Carbon::now(),
+            "updated_at" => Carbon::now(),
+        ]);
     }
 }
